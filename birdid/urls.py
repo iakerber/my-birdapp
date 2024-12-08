b196e7fda72a706b09ab29292201827ae31975af
ilze <iakerbergs@ivytech.edu>
2024-12-08 17:02:00 -0500

    Added view and template for detailed waterbird entry as well as CSS for the site.

--- birdid/urls.py
+++ birdid/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.waterbird_list, name='waterbird_list'),
+    path('waterbird/<int:pk>/', views.waterbird_detail, name='waterbird_detail'),
 ]
--- birdid/views.py
+++ birdid/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Waterbird
 
 # Create your views here.
 def waterbird_list(request):
     waterbirds = Waterbird.objects.all()
     return render(request, 'birdid/waterbird_list.html', {'waterbirds': waterbirds})
+
+def waterbird_detail(request, pk):
+    waterbird = get_object_or_404(Waterbird, pk=pk)
+    return render(request, 'birdid/waterbird_detail.html', {'waterbird': waterbird})
